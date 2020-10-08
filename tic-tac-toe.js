01d06159e550e9643e8d0618b054c7f8cb2ea6b6
Javian Anderson <javian99anderson@gmail.com>
2020-10-08 00:40:59 -0500

    Updated JS file again to try show board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,52 +1,42 @@
-document.getElementById("board").classList.add("square");
+//document.getElementById("board").classList.add("square");
 
-const statusDisplay = document.querySelector('.game');
+//const statusDisplay = document.querySelector("#game");
 
-let currentPlayer = "X";
-let nextPlayer = "O";
+//let currentPlayer = "X";
+//let nextPlayer = "O";
 
-let gameState = ["", "", "", "", "", "", "", "", ""];
-
-
-document.addEventListener('DOMContentLoaded', function(){
-	var board = document.getElementById("board");
-	var boardl= board.length;
-
-	for(var i=0; i<18; i++){
-		document.getElementById("board").childNodes[i].className='square';
-		
-	}
-    var state = [];
-    var players = ["X","O"];
-    var currentTurn = 
-
-    document.getElementsbyClass('square').addEventListener('click',function(){
-    	
-
-    })
-
-    for (var i=0; i<18; i++) {
-    	var j = document.getElementById("board").childNodes[i];
-    	j.addEventListener("click",function(){
-    	    	if(state.length==0){
-	    		j.classList.add('X');
-	    		console.log(j.innerHTML=('X'));
-	    		state.push('X');
-	    	}else{
-	    		if (state[(state.length-1)]=='X'){
-	    			j.innerHTML=('O');
-	    			state.push('O');
-	    		}else{
-	    			j.classList.add('X');
-	    			j.innerHTML=('X');
-	    			state.push('X')
-			}
-		}
-		});
-    };
+//let gameState = ["", "", "", "", "", "", "", "", ""];
+//var boxNum = 0
 
 
-    
-    
-  
-}) 
+function gameplay(){
+    var board = document.querySelector("#board");
+	var boxes = board.querySelectorAll("div");
+	var empty ='';
+	var tracker=0;
+	var boxNum=0;
+	var x_O;
+	var stat = document.querySelector("#status");
+	var newButton = document.querySelector("button");
+
+	boxes.forEach(element =>{
+		element.setAttribute("class","square");
+		element.textContent = empty;
+		element.setAttribute("id",boxNum++);
+	});
+}
+
+window.onload=gameplay;
+
+
+//const render_board = () => {
+//    statusDisplay.innerHTML = "";
+ //   gameState.forEach((e,i)) => {
+ //       statusDisplay.innerHTML += '<div class="square" onclick="addPlayerMove(${i})">${gameState[i]}</div>';
+//        if (e == currentPlayer || e == nextPlayer) {
+//            document.querySelector("#board").classList.add("occupied");
+//        }
+//    });
+//};
+
+// render_board();
