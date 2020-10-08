060c690f3e444611fc219f617ccbadc097560339
Javian Anderson <javian99anderson@gmail.com>
2020-10-08 00:02:12 -0500

    Updated JS file again to try show board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -18,3 +18,46 @@ const render_board = () => {
 };
 
 render_board();
+
+document.addEventListener('DOMContentLoaded', function(){
+	var board = document.getElementById("board");
+	var boardl= board.length;
+
+	for(var i=0; i<18; i++){
+		document.getElementById("board").childNodes[i].className='square';
+		
+	}
+    var state = [];
+    var players = ["X","O"];
+    var currentTurn = 
+
+    document.getElementsbyClass('square').addEventListener('click',function(){
+    	
+
+    })
+
+    for (var i=0; i<18; i++) {
+    	var j = document.getElementById("board").childNodes[i];
+    	j.addEventListener("click",function(){
+    	    	if(state.length==0){
+	    		j.classList.add('X');
+	    		console.log(j.innerHTML=('X'));
+	    		state.push('X');
+	    	}else{
+	    		if (state[(state.length-1)]=='X'){
+	    			j.innerHTML=('O');
+	    			state.push('O');
+	    		}else{
+	    			j.classList.add('X');
+	    			j.innerHTML=('X');
+	    			state.push('X')
+			}
+		}
+		});
+    };
+
+
+    
+    
+  
+}) 
