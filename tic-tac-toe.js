b0db7ab79a127a1490bc92d01947e7cc7b94b929
Javian Anderson <javian99anderson@gmail.com>
2020-10-08 00:04:12 -0500

    Updated JS file again to try show board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -7,17 +7,6 @@ let nextPlayer = "O";
 
 let gameState = ["", "", "", "", "", "", "", "", ""];
 
-const render_board = () => {
-    statusDisplay.innerHTML = "";
-    gameState.forEach((e,i)) => {
-        statusDisplay.innerHTML += '<div class="square" onclick="addPlayerMove(${i})">${gameState[i]}</div>';
-        if (e == currentPlayer || e == nextPlayer) {
-            document.querySelector('.board').classList.add("occupied");
-        }
-    });
-};
-
-render_board();
 
 document.addEventListener('DOMContentLoaded', function(){
 	var board = document.getElementById("board");
