6581044dfa7daff7d383162bde92e21a64d811d6
Javian Anderson <javian99anderson@gmail.com>
2020-10-07 23:23:15 -0500

    Exercise 1 Tried

---
+++ tic-tac-toe.js
@@ -0,0 +1,8 @@
+document.getElementById("board").classList.add("square");
+
+const statusDisplay = document.querySelector('.game');
+
+let currentPlayer = "X";
+
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
